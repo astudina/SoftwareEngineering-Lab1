0a4240c9e966c7af4b5d5a4971b0e8d1e2ddde59
astudina <nastya.astudina@mail.ru>
2021-11-29 22:11:50 +0300

    add ValueError checking

--- converter.py
+++ converter.py
@@ -1,8 +1,16 @@
 def converter(temperature):
     temperature = temperature.split(" ")
 
-    degree = int(temperature[0])
-    unit = temperature[1]
+    if len(temperature) != 2:
+        print("You should write two arguments")
+        exit()
+    else:
+        try:
+            degree = int(temperature[0])
+            unit = temperature[1]
+        except ValueError:
+            print("Incorrect input")
+            exit()
 
     if unit.upper() == "F":
         res_degree = int(round((degree - 32) * 5 / 9))
